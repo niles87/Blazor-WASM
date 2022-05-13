ce42bfd2026696a402e5f2a8da9a6a3c82e5de47
Niles Bingham <nbing19@gmail.com>
2022-05-12 21:43:21 -0700

    Add project files.

---
+++ Server/Controllers/BlogController.cs
@@ -0,0 +1,24 @@
+﻿using Blazor_WASM.Shared;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blazor_WASM.Server.Controllers;
+[ApiController]
+[Route("[controller]")]
+public class BlogController : ControllerBase {
+
+  private static readonly Blog[] fakeBlogs = new Blog[] {
+    new Blog() { Title = "New post", Content = "First Post", UserId = "1234", Username = "FirstUser" },
+    new Blog() { Title = "My Second post", Content = "Second One!!", UserId = "123", Username = "Second User" },
+    new Blog() { Title = "Hello world", Content = "Well hello world", UserId = "1234", Username = "FirstUser" }, };
+
+  private readonly ILogger<BlogController> _logger;
+
+  public BlogController(ILogger<BlogController> logger) {
+    _logger = logger;
+  }
+
+  [HttpGet]
+  public IEnumerable<Blog> Get() {
+    return fakeBlogs.ToArray();
+  }
+}
---
+++ Shared/Blog.cs
@@ -0,0 +1,9 @@
+﻿namespace Blazor_WASM.Shared {
+  public class Blog {
+    public DateTime Date { get; set; } = DateTime.Now;
+    public string Title { get; set; } 
+    public string Content { get; set; } 
+    public string Username { get; set; }
+    public string UserId { get; set; }
+  }
+}
